d1a8411973063d9d04e98451cdb8449854002c25
Din Yair <dinsadot@gmail.com>
2022-04-24 23:53:28 +0300

    4th video added in videos page

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -13,21 +13,27 @@ const Videos = () => {
         
         <div className='videos__item'>
           <h2 className='heading-2 videos__title'>Psychedelic Days</h2>
-          <h5 className='heading-4'>An original art journal made in the qurantine days of 2020</h5>
+          <h4 className='heading-4'>An original art journal made in the qurantine days of 2020</h4>
           <YouTubeVideo link="https://www.youtube.com/embed/nt2idwDFzH0"/>
         </div>
 
 
         <div className='videos__item reveal'>
           <h2 className='heading-2 videos__title'>Wabi Sabi - Mixed Media</h2>
-          <h5 className='heading-4'>A short demonstration of one work</h5>
+          <h4 className='heading-4'>A 3 minutes demonstration of a work</h4>
           <YouTubeVideo link="https://www.youtube.com/embed/PUjYCkNugtg"/>
+        </div>
+
+        <div className='videos__item reveal'>
+          <h2 className='heading-2 videos__title'>Bubble birds junk journal</h2>
+          <h4 className='heading-4'>Illustrated random pages binded together</h4>
+          <YouTubeVideo link="https://www.youtube.com/embed/vxjMYSLMai8"/>
         </div>
 
 
         <div className='videos__item reveal'>
           <h2 className='heading-2 videos__title'>The Spiritual Trilogy 1</h2>
-          <h5 className='heading-4'>A taste of spiritual collage course</h5>
+          <h4 className='heading-4'>Pictures from the first course of the trilogy</h4>
           <YouTubeVideo link="https://www.youtube.com/embed/SxPdd_t_G-4"/>
         </div>
 
