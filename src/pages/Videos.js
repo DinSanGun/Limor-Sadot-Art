e28e0254f885b5ba8e73a44aae6be1742f23a438
Din Yair Sadot <dinsadot@gmail.com>
2026-01-08 16:23:13 +0200

    add hebrew-arabic sketchbook video to 'videos'

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -38,6 +38,14 @@ const Videos = () => {
         </div>
 
 
+        <div className='videos__item reveal'>
+          <h2 className='heading-2 videos__title'>Hebrew-Arabic Sketchbook</h2>
+          <h4 className='heading-4'>An Illustrated sketchbook on old textbook</h4>
+          <YouTubeVideo link="https://www.youtube.com/watch?v=gfPaNHGUaVg"/>
+        </div>
+
+
+
       </div>
 
       <div className='videos__footer'>
