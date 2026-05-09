1f00a2889ea7a160f3adb592998904802bd3a171
Din Yair Sadot <dinsadot@gmail.com>
2026-05-09 18:44:15 +0300

    refactor: load The Purple Era gallery from shared data

--- src/galleries/ThePurpleEra.js
+++ src/galleries/ThePurpleEra.js
@@ -1,42 +1,24 @@
 import Gallery from '../pages/Gallery';
-
-const smallImagesContext = require.context(
-  '../img/collage/the-purple-era/small',
-  false,
-  /\.jpg$/
-);
-
-const bigImagesContext = require.context(
-  '../img/collage/the-purple-era/big',
-  false,
-  /\.jpg$/
-);
-
-const extractImageNumber = (fileName) => {
-  const match = fileName.match(/^\.\/(\d+)/);
-  return match ? Number(match[1]) : 0;
-};
-
-const sortImages = (a, b) => {
-  return extractImageNumber(a) - extractImageNumber(b);
-};
-
-const smallImages = smallImagesContext.keys().sort(sortImages);
-const bigImages = bigImagesContext.keys().sort(sortImages);
-
-const ThePurpleEraGallery = {
-  title: 'The Purple Era',
-  description: 'mixed media paintings',
-
-  images: smallImages.map((smallImagePath, index) => ({
-    id: index + 1,
-    smallSrc: smallImagesContext(smallImagePath),
-    bigSrc: bigImagesContext(bigImages[index]),
-  })),
-};
+import { getGalleryBySlug } from '../data/galleries';
+import { getGalleryImages } from '../data/galleryImages';
 
 const ThePurpleEra = () => {
-  return <Gallery grid="3" series={ThePurpleEraGallery} />;
+  const gallery = getGalleryBySlug('the-purple-era');
+  const images = gallery ? getGalleryImages(gallery) : [];
+
+  const series = gallery
+    ? {
+        title: gallery.title,
+        description: gallery.description,
+        images,
+      }
+    : {
+        title: 'The Purple Era',
+        description: 'mixed media paintings',
+        images,
+      };
+
+  return <Gallery grid={gallery?.grid ?? '3'} series={series} />;
 };
 
 export default ThePurpleEra;
