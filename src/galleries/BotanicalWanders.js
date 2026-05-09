5237f659d379284cc08f36fb6e8842532f920ca0
Din Yair Sadot <dinsadot@gmail.com>
2026-05-09 19:02:32 +0300

    refactor: load all collage galleries from shared data

--- src/galleries/BotanicalWanders.js
+++ src/galleries/BotanicalWanders.js
@@ -1,102 +1,24 @@
-import sImage1  from '../img/collage/botanical-wanders/small/01-botanicollage001A_small.jpg'
-import sImage2  from '../img/collage/botanical-wanders/small/01-botanicollage002A_small.jpg'
-import sImage3  from '../img/collage/botanical-wanders/small/01-botanicollage003A_small.jpg'
-import sImage4  from '../img/collage/botanical-wanders/small/01-botanicollage004A_small.jpg'
-import sImage5  from '../img/collage/botanical-wanders/small/01-botanicollage005A_small.jpg'
-import sImage6  from '../img/collage/botanical-wanders/small/01-botanicollage006A_small.jpg'
-import sImage7  from '../img/collage/botanical-wanders/small/01-botanicollage007A_small.jpg'
-import sImage8  from '../img/collage/botanical-wanders/small/01-botanicollage008A_small.jpg'
-import sImage9  from '../img/collage/botanical-wanders/small/01-botanicollage009A_small.jpg'
-import sImage10 from '../img/collage/botanical-wanders/small/01-botanicollage010A_small.jpg'
-import sImage11 from '../img/collage/botanical-wanders/small/01-botanicollage011A_small.jpg'
-import sImage12 from '../img/collage/botanical-wanders/small/01-botanicollage012A_small.jpg'
-
-import bImage1  from '../img/collage/botanical-wanders/big/01-botanicollage001A_big.jpg'
-import bImage2  from '../img/collage/botanical-wanders/big/01-botanicollage002A_big.jpg'
-import bImage3  from '../img/collage/botanical-wanders/big/01-botanicollage003A_big.jpg'
-import bImage4  from '../img/collage/botanical-wanders/big/01-botanicollage004A_big.jpg'
-import bImage5  from '../img/collage/botanical-wanders/big/01-botanicollage005A_big.jpg'
-import bImage6  from '../img/collage/botanical-wanders/big/01-botanicollage006A_big.jpg'
-import bImage7  from '../img/collage/botanical-wanders/big/01-botanicollage007A_big.jpg'
-import bImage8  from '../img/collage/botanical-wanders/big/01-botanicollage008A_big.jpg'
-import bImage9  from '../img/collage/botanical-wanders/big/01-botanicollage009A_big.jpg'
-import bImage10 from '../img/collage/botanical-wanders/big/01-botanicollage010A_big.jpg'
-import bImage11 from '../img/collage/botanical-wanders/big/01-botanicollage011A_big.jpg'
-import bImage12 from '../img/collage/botanical-wanders/big/01-botanicollage012A_big.jpg'
-
-import Gallery from '../pages/Gallery'
+import Gallery from '../pages/Gallery';
+import { getGalleryBySlug } from '../data/galleries';
+import { getGalleryImages } from '../data/galleryImages';
 
 const BotanicalWanders = () => {
+  const gallery = getGalleryBySlug('botanical-wanders');
+  const images = gallery ? getGalleryImages(gallery) : [];
 
-    const BotanicalWandersGallery = {
-        title: "Botanical Wanders",
-        description: "leafy bloomy dark collage",
-        images: 
-        [
-            {
-                id: 1,
-                smallSrc: sImage1,
-                bigSrc: bImage1
-            },
-            {
-                id: 2,
-                smallSrc: sImage2,
-                bigSrc: bImage2
-            },
-            {
-                id: 3,
-                smallSrc: sImage3,
-                bigSrc: bImage3
-            },
-            {
-                id: 4,
-                smallSrc: sImage4,
-                bigSrc: bImage4
-            },
-            {
-                id: 5,
-                smallSrc: sImage5,
-                bigSrc: bImage5
-            },
-            {
-                id: 6,
-                smallSrc: sImage6,
-                bigSrc: bImage6
-            },
-            {
-                id: 7,
-                smallSrc: sImage7,
-                bigSrc: bImage7
-            },
-            {
-                id: 8,
-                smallSrc: sImage8,
-                bigSrc: bImage8
-            },
-            {
-                id: 9,
-                smallSrc: sImage9,
-                bigSrc: bImage9
-            },
-            {
-                id: 10,
-                smallSrc: sImage10,
-                bigSrc: bImage10
-            },
-            {
-                id: 11,
-                smallSrc: sImage11,
-                bigSrc: bImage11
-            },
-            {
-                id: 12,
-                smallSrc: sImage12,
-                bigSrc: bImage12
-            }
-        ]
-    }
+  const series = gallery
+    ? {
+        title: gallery.title,
+        description: gallery.description,
+        images,
+      }
+    : {
+        title: 'Botanical Wanders',
+        description: 'leafy bloomy dark collage',
+        images,
+      };
 
-  return <Gallery grid="2" series={ BotanicalWandersGallery } />
-}
+  return <Gallery grid={gallery?.grid ?? '2'} series={series} />;
+};
 
-export default BotanicalWanders
+export default BotanicalWanders;
--- src/galleries/WhimsicalDarkness.js
+++ src/galleries/WhimsicalDarkness.js
@@ -1,42 +1,24 @@
 import Gallery from '../pages/Gallery';
-
-const smallImagesContext = require.context(
-  '../img/collage/whimsical-darkness/small',
-  false,
-  /\.jpg$/
-);
-
-const bigImagesContext = require.context(
-  '../img/collage/whimsical-darkness/big',
-  false,
-  /\.jpg$/
-);
-
-const extractImageNumber = (fileName) => {
-  const match = fileName.match(/^\.\/(\d+)/);
-  return match ? Number(match[1]) : 0;
-};
-
-const sortImages = (a, b) => {
-  return extractImageNumber(a) - extractImageNumber(b);
-};
-
-const smallImages = smallImagesContext.keys().sort(sortImages);
-const bigImages = bigImagesContext.keys().sort(sortImages);
-
-const WhimsicalDarknessGallery = {
-  title: 'Whimsical Darkness',
-  description: 'analogue paper collage',
-
-  images: smallImages.map((smallImagePath, index) => ({
-    id: index + 1,
-    smallSrc: smallImagesContext(smallImagePath),
-    bigSrc: bigImagesContext(bigImages[index]),
-  })),
-};
+import { getGalleryBySlug } from '../data/galleries';
+import { getGalleryImages } from '../data/galleryImages';
 
 const WhimsicalDarkness = () => {
-  return <Gallery grid="3" series={WhimsicalDarknessGallery} />;
+  const gallery = getGalleryBySlug('whimsical-darkness');
+  const images = gallery ? getGalleryImages(gallery) : [];
+
+  const series = gallery
+    ? {
+        title: gallery.title,
+        description: gallery.description,
+        images,
+      }
+    : {
+        title: 'Whimsical Darkness',
+        description: 'analogue paper collage',
+        images,
+      };
+
+  return <Gallery grid={gallery?.grid ?? '3'} series={series} />;
 };
 
 export default WhimsicalDarkness;
